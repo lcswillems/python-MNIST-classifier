81cacc1b73214e0750c061732de51b25b31ebab6
Lucas Willems <lcswillems@gmail.com>
2018-04-22 12:17:20 -0400

    Simplification du main

--- code/main.py
+++ code/main.py
@@ -1,71 +1,56 @@
 #!/usr/bin/env python3
 
-from tensorflow.python import keras
 from tensorflow.python.keras.optimizers import SGD, Adam
-from tensorflow.python.keras.callbacks import TensorBoard
+import numpy as np
 
 import arguments
-import data
-import models
 import ui
-
-def load_model(name):
-    try:
-        global MODEL_LOCATION
-
-        MODEL_LOCATION = "storage/" + name + ".h5"
-        return keras.models.load_model(MODEL_LOCATION)
-    except OSError:
-        return getattr(models, name)()
-
-def save_model(model):
-    def create_file_if_not_exists(file_location):
-        import os
-
-        os.makedirs(os.path.dirname(file_location), exist_ok=True)
-        with open(file_location, "w"):
-            pass
-
-    global MODEL_LOCATION
-
-    create_file_if_not_exists(MODEL_LOCATION)
-    keras.models.save_model(model, MODEL_LOCATION)
-
-def train_model(model, optimizer, epochs):
-    (x_train, y_train), (_, _) = data.get()
-
-    model.compile(loss='categorical_crossentropy',
-                  optimizer=optimizer,
-                  metrics=['accuracy'])
-
-    model.fit(x=x_train,
-              y=y_train,
-              epochs=epochs,
-              batch_size=128)
-    
-    save_model(model)
-
-def test_model(model):
-    (_, _), (x_test, y_test) = data.get()
-
-    score = model.evaluate(x_test, y_test, batch_size=128)
-    print("Accuracy: {}%".format(round(score[1]*100, 1)))
-
-def use_model(model):
-    ui.build(model)
+import utils
 
 args = arguments.get_args()
-assert int(args.train) + int(args.test) + int(args.use) == 1
 
-model = load_model(args.model)
+model = utils.load_model(args.model)
+model.summary()
 
-if args.train:
-    lr = 1e-4 if args.lr == None else args.lr
-    optimizer = Adam(lr=lr) if args.optimizer == "adam" else SGD(lr=lr)
-    train_model(model, optimizer, args.epochs)
+if args.mode == "train":
+    if args.optimizer == "sgd":
+        optimizer = SGD(lr=args.lr)
+    elif args.optimizer == "adam":
+        optimizer = Adam(lr=args.lr)
+    else:
+        raise ValueError("Nom d'optimiseur inconnu")
 
-elif args.test:
-    test_model(model)
+    (x_train, y_train), (_, _) = utils.get_data()
 
-elif args.use:
-    use_model(model)
+    model.compile(loss="categorical_crossentropy",
+                  optimizer=optimizer,
+                  metrics=["accuracy"])
+    model.fit(x=x_train,
+              y=y_train,
+              epochs=args.epochs,
+              batch_size=128)
+    
+    utils.save_model(model, args.model)
+
+elif args.mode == "test":
+    (_, _), (x_test, y_test) = utils.get_data()
+    cls_test = np.argmax(y_test, axis=1)
+
+    y_pred = model.predict(x_test, batch_size=128, verbose=1)
+    cls_pred = np.argmax(y_pred, axis=1)
+
+    incorrect = (cls_pred != cls_test)
+
+    accuracy = 1 - np.mean(incorrect)
+    accuracy = round(accuracy*100, 1)
+    print("Accuracy: {}%".format(accuracy))
+
+    utils.plot_images(images=x_test[incorrect],
+                      cls_true=cls_test[incorrect],
+                      cls_pred=cls_pred[incorrect])
+
+elif args.mode == "use":
+    ui.build(model)
+
+else:
+    raise ValueError("Mode invalide")
