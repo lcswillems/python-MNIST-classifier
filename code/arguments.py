741a971c79457eafe496dd934434ca7d28f258ce
Lucas Willems <lcswillems@gmail.com>
2018-10-13 11:55:18 +0200

    Amélioration des paramètres

--- code/arguments.py
+++ code/arguments.py
@@ -9,7 +9,7 @@ def get_args():
     )
     parser.add_argument(
         "--mode",
-        help="""Mode d'utilisation : 'train', 'test' ou 'use' (OBLIGATOIRE)
+        help="""Mode d'utilisation du programme : 'train', 'test' ou 'use' (OBLIGATOIRE)
     - train: Entraîne le modèle sur les données d'entraînement
     - test: Teste le modèle sur les données de test
     - use: Utilise le modèle pour classifier de nouveaux chiffres""",
@@ -17,36 +17,36 @@ def get_args():
     )
     parser.add_argument(
         "--model",
-        help="Nom du modèle (ou classe de généralisations) (défaut: deepnet1)",
+        help="Nom du modèle : 'deepnet1', 'deepnet2', 'convnet' (défaut: deepnet1)",
         default="deepnet1"
     )
     parser.add_argument(
+        "--minimizer",
+        help="Nom du minimiseur : 'sgd' ou 'adam' (défaut: adam)",
+        default="adam"
+    )
+    parser.add_argument(
         "--epochs",
-        help="Nombre d'entraînement du modèle sur toutes les données d'entraînement (défaut: 1)",
+        help="Nombre de minimisations (défaut: 1)",
         type=int,
         default=1
     )
     parser.add_argument(
-        "--optimizer",
-        help="Nom de l'optimiseur pour entraîner le modèle : 'sgd' ou 'adam' (défaut: adam)",
-        default="adam"
-    )
-    parser.add_argument(
         "--lr",
-        help="Learning rate utilisé par l'optimiseur (défaut: 0.0002)",
+        help="Learning rate utilisé par le minimiseur (défaut: 0.0002)",
         type=float,
         default=2e-4
     )
     parser.add_argument(
-        "--batch-size",
-        help="Nombre d'exemples utilisés pour chaque minimisation (défaut: 128)",
-        type=int,
-        default=128
-    )
-    parser.add_argument(
         "--examples",
-        help="Nombre de données d'entraînement (défaut: toutes i.e. 60000)",
+        help="Nombre de données utilisées pour l'entraînement (défaut: toutes i.e. 60000)",
         type=int,
         default=None
+    )
+    parser.add_argument(
+        "--batch-size",
+        help="(pour les connaisseurs) (défaut: 128)",
+        type=int,
+        default=128
     )
     return parser.parse_args()
